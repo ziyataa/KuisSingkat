a815346c17df65994e8f027b25a039da27b883eb
ilham <ziyatakhsani@gmail.com>
2018-11-29 13:48:14 +0700

    update

--- app/src/main/java/com/ziyata/kuissingkat/MainActivity.java
+++ app/src/main/java/com/ziyata/kuissingkat/MainActivity.java
@@ -3,6 +3,9 @@ package com.ziyata.kuissingkat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -46,6 +49,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected (MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.About:
+                Intent pindah = new Intent(this, About.class);
+                startActivity(pindah);
+                break;
+            case R.id.Profile:
+                pindah = new Intent(this, Profile.class);
+                startActivity(pindah);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
